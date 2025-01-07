945d652ac30295d9e3f71d40130343ff33bb7a87
erickunx <erickunx@gmail.com>
2025-01-06 21:17:31 -0800

    set up the file for the guessing calculator before testing

---
+++ guessing_game/target/debug/deps/guessing_game-10a943f44aba3491.d
@@ -0,0 +1,5 @@
+/home/hutao/Downloads/rust/guessing_game/target/debug/deps/guessing_game-10a943f44aba3491: src/main.rs
+
+/home/hutao/Downloads/rust/guessing_game/target/debug/deps/guessing_game-10a943f44aba3491.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/hutao/Downloads/rust/guessing_game/target/debug/guessing_game: /home/hutao/Downloads/rust/guessing_game/src/main.rs
