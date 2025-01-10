74f9f7e8092f1e50c82cb68a63bd2834e2597686
erickunx <erickunx@gmail.com>
2025-01-10 13:24:23 -0800

    implement random number generator feature and generate a new random number set to the varaible secret_number each time the program is run

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,40 @@
 use std::io;    //by default rust has a lot of built in defined as the standard library which is the prelude, if
-                //a type you wnant is not included you can add that type into the scope by using
-                //the use statement
+                    //a type you wnant is not included you can add that type into the scope by using
+                    //the use statement
                 //std means that it is from the standard library
                 //io is a library that provides you with features that include the ability to
-                //accept user input
+                    //accept user input
+
+use rand::Rng;  //Rng defines methods thar random number generators implements and for a random number to be 
+                    //generated this is needed
+                // you will not know what traits to use adn which methods and functions to call
+                    // from a crate so each crate has documentation with instructions for using them
+                    // you can run "cargo doc --open" which will build documentation provided by
+                    // all the dependencies locally and open it in your browser   
 
 //fn declares a new function 
 //() means that there are no parameters
 fn main() {
     println!("Guess the number!");      //println! is a macro that prints a string to the screen
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);      //rand::thread_rng is the specific random
+                                                                        //number generator that is
+                                                                        //used and is made possible
+                                                                        //by using Rng trait that
+                                                                        //was brought in by "use
+                                                                        //rand:: Rng"
+                                                                    //"gen_range" takes a range
+                                                                        //expression as an argument and
+                                                                        //generates a random number
+                                                                        //in that range in the form
+                                                                        //of "{start}..={end}" and
+                                                                        //is inclusive on the lower
+                                                                        //and upper bounds so
+                                                                        //"1..=100" is numbers
+                                                                        //between 1 and 100 
+    
+    println!("The secret number is: {secret_number}");      //prints the secret number {} allows for printing
+                                                                //of variables
 
     println!("Please input your guess.");
 
@@ -18,13 +44,10 @@ fn main() {
                                         //all together, this line creates a mutable varianle that
                                         //is bound to a new empty instance of a String
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-        //above line can be written as
-        //io::stdin().read_line(&mut guess).expect("failed to read line);
-        //but this is harder to read therefore it is split into three lines above
+    io::stdin()                         //these line can be written as
+        .read_line(&mut guess)          //io::stdin().read_line(&mut guess).expect("failed to read li
+        .expect("Failed to read line"); //but this is harder to read therefore it is split into three lines
+    `
     println!("You guessed: {}", guess); //current print out of what the guess is 
 
 }
